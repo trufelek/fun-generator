7b695391ae2c6d08102e3ba48ec46bc77f7b3251
Aleksandra Wierzbiak <alewierzbiak@gmail.com>
2017-05-07 15:51:36 +0200

    registration

---
+++ js/register.js
@@ -0,0 +1,28 @@
+$(function() {
+    $("#register-form").validate({
+        rules: {
+            nick: "required",
+            password: {
+                required: true,
+                minlength: 6
+            },
+            password2: {
+                required: true,
+                minlength: 6,
+                equalTo: "#register-password"
+            }
+        },
+        messages: {
+            nick: "Podaj swój login",
+            password: {
+                required: "Podaj hasło",
+                minlength: "Hasło musi mieć co najmniej 6 znaków"
+            },
+            password2: {
+                required: "Podaj hasło",
+                minlength: "Hasło musi mieć co najmniej 6 znaków",
+                equalTo: "Hasła muszą się zgadzać."
+            }
+        }
+    });
+});
